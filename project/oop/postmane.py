59e25d6814cffb22c3d87d7903193b9c79d6e1a4
Tryd0g0lik <work80@mail.ru>
2024-06-28 09:58:23 +0700

    the code was cleaned

--- project/oop/postmane.py
+++ project/oop/postmane.py
@@ -37,26 +37,15 @@ class Postmen(BasicBranches):
 	async def sorter_data(self, *args):
 		list_total_data = []
 
-		list_total_data.append(args[0])
-		# list_total_data.append(list(args)[1][1])
+		# list_total_data.append(args[0])
 
+		''' Refresh variables: sisyphus_packages / packages_p10 '''
 		# sisyphus_packages = args[0][0]
 		packages_sisyphus = PACKAGES_SISYPHUS
 		# packages_p10 = args[0][1]
 		packages_p10 = PACKAGES_P10
 		result = await Postmen.compare_version_release(packages_sisyphus, packages_p10)
 
-		# list_sisyphus = list_total_data[0].copy()
-		# list_p10 = list_total_data[1].copy()
-		new_list_sisyphus = []
-		# sisyphus_packages
-		# for sisyphus in sisyphus_packages:
-		#
-		# for name, sisyphus_packages in sisyphus_packages.items():
-		# 	name = 'name'
-		# 	# if name == sisyphus_package[0] in sisyphus_package:
-		# 	# 	pass
-		# 	pass
 
 	@staticmethod
 	async def compare_version_release(package1:list, package2:list) -> list:
@@ -64,17 +53,11 @@ class Postmen(BasicBranches):
 		result_package2 = []
 		result = []
 
-		result_package1:list = await Postmen.search_list(package1, package2)
+		result_package1:list = await Postmen.search_list(package1.copy(), package2.copy())
 		result.append(result_package1)
-		result_package2:list = await Postmen.search_list(package2, package1)
+		result_package2:list = await Postmen.search_list(package2.copy(), package1.copy())
 		result.append(result_package2)
 		return result
-
-				# result_package1.append(single)
-
-				# for single_dict_ofPackage2 in package2:
-				# 	if ('name:' + str(single['name'])) in single_dict_ofPackage2:
-				# 		single_dict_ofPackage2
 
 	@staticmethod
 	async def search_list(list_analog:list, list_toSearch:list ) -> list:
@@ -92,22 +75,12 @@ class Postmen(BasicBranches):
 			list_bool = [first_list['name'] not in subsingle['name'] for subsingle in list_toSearch]
 			if list_bool.count(False) == 0:
 				return first_list
+
 			return False
 
 		for single in list_analog:
 			result = await subsearch_list(single)
 			if type(result) != bool:
 				result_package.append(result)
-			# for subsingle in list_toSearch:
-			# 	if list_bool.count(False) > 0:
-			# 		continue
-			# 		# break
-			#
-			# 	if single['name'] not in subsingle['name']:
-			# 		list_bool.append(True)
-			# 		# result_package.append(single)
-			# 	else:
-			# 		list_bool.append(False)
-			#
-			# 	result_package.append(single)
+
 		return result_package
